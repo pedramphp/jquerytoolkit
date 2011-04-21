9f3ae53017e83c7ccf94b26b27d785979a06775d
mahdi pedram <pedramphp@gmail.com>
2011-04-20 18:14:44 -0700

    Adding Under Construction page fixing some broken in links in the footer, also fixing some broken styles and html in footer. updating the htacess

---
+++ actions/404.php
@@ -0,0 +1,15 @@
+<?php 
+
+require_once(LiteFrame::GetFileSystemPath()."includes/Site.class.php");
+
+class LiteFrameAction{
+	
+	public function __construct(){
+		
+		$site = new Site();
+	}
+	
+}
+
+
+?>
